b2a4b2c016d7aa79bedd99f83f932deddc3f28a4
James NI <SESA463244@schneider-electric.com>
2018-02-13 17:37:55 +0800

    initial backend setup

---
+++ backend/src/test/java/com/ningning/demo/CaloriesTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ningning.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CaloriesTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
