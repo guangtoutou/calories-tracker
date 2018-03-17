795140e699dd3051100220f5a5fb0791534214a6
guangtoutou <ni.ningning@gmail.com>
2018-03-18 07:48:30 +0800

    delete frontend folder manually built using webpack

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default {
-	user:{
-		login: credentials =>
-			axios.post('http://localhost:8080/auth',credentials)
-			.then(res => res.headers['authorization'])
-	}
-};
--- frontend/src/components/messages/confirmEmailMessage.js
+++
@@ -1,9 +0,0 @@
-import {Message} from 'semantic-ui-react'
-
-const ConfirmEmailMessage = ({text}) => (
-    <Message info>
-        <Message.Header>Please, verify your eamil to unlock awesomeness!</Message.Header>
-    </Message>
-)
-
-export default ConfirmEmailMessage;
