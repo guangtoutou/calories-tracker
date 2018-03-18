813faeaaa9757f717ea65dc7bbb38487ab5f52e7
guangtoutou <ni.ningning@gmail.com>
2018-03-18 21:36:58 +0800

    set up user route and guest route

--- front/src/App.js
+++ front/src/App.js
@@ -3,12 +3,14 @@ import {Route} from 'react-router-dom';
 import HomePage from './components/pages/homepage';
 import LoginPage from './components/pages/loginpage';
 import DashBoard from './components/pages/dashboard';
+import UserRoute from './components/routes/UserRoute';
+import GuestRoute from './components/routes/GuestRoute';
 
 const App = ()=>(
 	<div className='ui container'>
 		<Route path='/' exact component={HomePage}/>
-		<Route path='/login' exact component={LoginPage}/>
-		<Route path='/dashboard' exact component={DashBoard}/>
+		<GuestRoute path='/login' exact component={LoginPage}/>
+		<UserRoute path='/dashboard' exact component={DashBoard}/>
 		
 	</div>
 );
